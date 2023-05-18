2001b515f505f9f044f8c1e9e0cb972318536ade
Roosterbear <lfpereag@gmail.com>
2023-05-17 20:44:53 -0600

    Adding jest tests

---
+++ testings/test/demo2.test.js
@@ -0,0 +1,9 @@
+describe('Probando modulo ficticio <Describe />', ()=>{
+  test('Esto no debe fallar como BIC', ()=>{
+   const message1 = 'Hola Mundo';
+
+   const message2 = message1.trim();
+
+   expect( message1 ).toBe( message2 );
+  });
+});
