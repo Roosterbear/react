e207d272aa8a63179b27a5c8162b8f6147c0b6e1
Roosterbear <lfpereag@gmail.com>
2023-06-20 19:50:13 -0600

    finishing

--- useref/src/components/initial.js
+++ useref/src/components/initial.js
@@ -1,14 +1,28 @@
+// useRef: used to inputs
+// useState: to manage data or 'state' of a component
 import {useRef, useState} from 'react'
 
 const Initial = ()=>{
+  // A reference to useRef, where we are going to store the user data
   const textInput = useRef();
-  const [showNext, setShowNext] = useState(true);
+  // In the state, we need 2 values: the state and the set
+  // Look that we need a INITIAL state between parenthesis (false)
+  const [showNext, setShowNext] = useState(false);
 
+  const handleChange = ()=>{
+    if(textInput.current.value.length >= 5) setShowNext(true)
+    else setShowNext(false);
+  }
+
+
+  // Now create a validation to launch an error
+  // Print the text in a div with other text on button click
   return(
     <div>
       <h1>Ask a question</h1>
       <input
         ref={textInput}
+        onChange={handleChange}
         name="question"
         type="text"
         className="form-control"
