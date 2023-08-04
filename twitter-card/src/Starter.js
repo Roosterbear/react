8ff21feb2d99fac8c52322661bb8d6948f00819a
Roosterbear <lfpereag@gmail.com>
2023-08-03 23:08:36 -0600

    twitter-card starter

---
+++ twitter-card/src/Starter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Starter(){
+  
+  return(
+    <>
+      <div className='book'>
+        <div className='title'>
+          The Title
+        </div>
+        <div className='author'>
+          The Author
+        </div>
+        <ul className='starts'>
+          <li className='raiting'>
+            5 stars
+          </li>
+          <li className='isbn'>
+            12-345678-910
+          </li>
+        </ul>
+      </div>
+
+    </>
+  )
+}
+
+export default Starter
---
+++ twitter-card/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Starter from './Starter';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Starter />
+  </React.StrictMode>
+);
